73f65c6a4e409159d716f99c389c4f160554f586
Nick Battista <nickbattista@Nick.Battista>
2021-01-07 07:20:12 -0700

    error messaging for invalid currencies added

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from forex_python.converter import CurrencyRates, CurrencyCodes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "123abc!"
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 @app.route("/")
 def home_page():
@@ -14,15 +13,35 @@ def home_page():
 def convert():
     cr = CurrencyRates()
     cc = CurrencyCodes()
-    from_currency = request.form["from_currency"]
-    to_currency = request.form["to_currency"]
-    amount = int(request.form["amount"])
-    symbol = cc.get_symbol(to_currency)
-    result = cr.convert(from_currency, to_currency, amount)
-    return render_template("/results.html", symbol=symbol, result=result, from_currency=from_currency, to_currency=to_currency)
 
-@app.errorhandler(500)
-def handle_error(e):
-    # note that we set the 500 status explicitly
-    flash("Invalid currency code.")
-    return render_template("/index.html"), 500
+    from_currency = request.form["from_currency"].upper()
+    to_currency = request.form["to_currency"].upper()
+    valid_from_cn = cc.get_currency_name(from_currency)
+    valid_to_cn = cc.get_currency_name(to_currency)
+    symbol = cc.get_symbol(to_currency)
+    amount = int(request.form["amount"])
+
+    try:
+        result = cr.convert(from_currency, to_currency, amount)
+    except:
+        if valid_from_cn == None:
+            flash(f"Currency unavailable: {from_currency}")
+            return render_template("/index.html")
+
+        if valid_to_cn == None:
+            flash(f"Currency unavailable: {to_currency}")
+            return render_template("/index.html")
+
+        if type(amount) != int:
+            flash("Please enter valid amount.")
+            return render_template("/index.html")
+
+
+    return render_template("/results.html", symbol=symbol, result=round(result,2), from_currency=from_currency, to_currency=to_currency)
+
+# The object returned from CurrencyCodes() has a get_currency_name() method 
+# that attempts to map a code to a name. If that returns None, the code is invalid. 
+# The object returned from CurrencyRates() includes a convert() method that will throw 
+# an exception if the conversion is invalid. If you check the results of the 
+# get_currency_name() method and wrap the convert call in a try-except, you can determine 
+# if the data is invalid and if 1 or more error messages should be displayed via flash.
